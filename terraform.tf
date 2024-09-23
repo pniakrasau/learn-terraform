db548c5ee83a5302d7fdc60931984a3fce404dcb
pniakrasau <pavel.nekrasof.upwork@gmail.com>
2024-09-23 11:24:21 +0200

    initial commit

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
-
-    workspaces {
-      name = "learn-terraform"
-    }
-  }
+#   cloud {
+#     organization = "niakrasau-test-app"
+#
+#     workspaces {
+#       name = "learn-terraform"
+#     }
+#   }
 
   required_providers {
     aws = {
